d79ce588c887d8b8bec2ecd0bff9cef073df69e0
Christian Molina <Christianmr92@gmail.com>
2025-07-05 15:27:11 +0000

    Approach EDA Final-Project

---
+++ 07-final-project/script.py
@@ -0,0 +1,14 @@
+from __future__ import division
+from datetime import datetime
+import pandas as pd
+from pandas import Series,DataFrame
+import numpy as np
+
+#Visualisation libraries
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+
+#finance libraries
+import yfinance as yf
